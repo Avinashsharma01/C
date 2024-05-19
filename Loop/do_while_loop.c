c6d4e26677d0d3cb232a8e48c17fb720192487d9
Avinash Sharma <Avinashsharma31384@gmail.com>
2024-05-20 00:27:32 +0530

    third commit

---
+++ Loop/do_while_loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    //this is the example does do while print at once before the conditon
+    int i=1;
+    do{
+        printf("%d\n",i);
+        i++;
+    }while(i>=10);
+
+
+
+//this is the another example of do while loop
+    int j=1;
+    do{
+        printf("%d\n",j);
+        j++;
+    }while(j<=10);
+
+
+    return (0);
+}
---
+++ Loop/odd_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    while(num!=10){
+        printf("Enter any number:- ");
+        scanf("%d",&num);
+    }
+
+    printf("This is correct.");
+    return (0);
+}
